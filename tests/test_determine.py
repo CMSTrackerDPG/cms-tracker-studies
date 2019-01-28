f626eabab44ad9e2834636ec544079a9b6e1f0ee
Peter Stein <peterstein@protonmail.com>
2019-01-28 18:06:03 +0100

    Fix determine_run_type

--- tests/test_determine.py
+++ tests/test_determine.py
@@ -1,4 +1,6 @@
-from trackerstudies.determine import determine_tracker_is_bad
+import numpy
+
+from trackerstudies.determine import determine_tracker_is_bad, determine_run_type
 
 
 def test_determine_is_bad():
@@ -14,3 +16,16 @@ def test_determine_is_bad():
     assert determine_tracker_is_bad("good", "bad", "good", "collisions")
     assert determine_tracker_is_bad("good", "good", "bad", "collisions")
     assert determine_tracker_is_bad("bad", "bad", "bad", "collisions")
+
+
+def test_determine_runtype():
+    assert numpy.isnan(determine_run_type("/Express/Commissioning2018/DQM", "Commissioning18"))
+    assert numpy.isnan(determine_run_type("/Global/Online/ALL", "Commissioning18"))
+
+    assert "cosmics" == determine_run_type("/Express/Cosmics2018/DQM", "Cosmics18")
+    assert "cosmics" == determine_run_type("/Global/Online/ALL", "Cosmics18")
+    assert "cosmics" == determine_run_type("/PromptReco/Cosmics18/DQM", "Cosmics18")
+
+    assert "collisions" == determine_run_type("/Express/Collisions2018/DQM", "Collisions18")
+    assert "collisions" == determine_run_type("/Global/Online/ALL", "Collisions18")
+    assert "collisions" == determine_run_type("/PromptReco/Collisions2018Commiss/DQM", "Collisions18")
--- trackerstudies/determine.py
+++ trackerstudies/determine.py
@@ -2,12 +2,15 @@ import numpy
 
 
 def determine_tracker_is_bad(pixel, strip, tracking, runtype):
-    return (
-        pixel.lower() != "good"
-        and runtype.lower() == "collisions"
-        or strip.lower() != "good"
-        or tracking.lower() != "good"
-    )
+    try:
+        return (
+            pixel.lower() != "good"
+            and runtype.lower() == "collisions"
+            or strip.lower() != "good"
+            or tracking.lower() != "good"
+        )
+    except AttributeError:
+        return numpy.nan
 
 
 def determine_is_collisions(run_class_name, rda_name):
@@ -15,7 +18,7 @@ def determine_is_collisions(run_class_name, rda_name):
 
 
 def determine_is_cosmics(run_class_name, rda_name):
-    return "Collisions" in run_class_name or "Collisions" in rda_name
+    return "Cosmics" in run_class_name or "Cosmics" in rda_name
 
 
 def determine_run_type(run_class_name, rda_name):
