789ed726a672758a0e8803476166dc7e30a5525e
Peter Stein <peterstein@protonmail.com>
2019-01-28 10:09:03 +0100

    Create trackerstudies package

---
+++ setup.py
@@ -0,0 +1,34 @@
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="trackerstudies",
+    version=find_version("trackerstudies", "__init__.py"),
+    desription="CERN CMS Tracker studies",
+    url="https://github.com/ptrstn/cms-tracker-studies",
+    author="Peter Stein",
+    author_email="peter.stein@cern.ch",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={"console_scripts": ["trackerstudies=trackerstudies.main:main"]},
+)
