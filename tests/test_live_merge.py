2940869e2a2a276436a17d06dd6edcec7d8d3922
Peter Stein <peterstein@protonmail.com>
2019-01-28 14:50:23 +0100

    Create merge functions

--- tests/test_live_merge.py
+++ tests/test_live_merge.py
@@ -1,5 +1,14 @@
-from trackerstudies.load import load_tracker_runs, load_global_runs
-from trackerstudies.merge import merge_runreg_runreg
+from trackerstudies.load import (
+    load_tracker_runs,
+    load_global_runs,
+    load_oms_runs,
+    load_tkdqmdoctor_runs,
+)
+from trackerstudies.merge import (
+    merge_runreg_runreg,
+    merge_runreg_oms,
+    merge_runreg_tkdqmdoc,
+)
 
 
 def test_merge_runreg_runreg():
@@ -9,15 +18,52 @@ def test_merge_runreg_runreg():
     tracker_size = len(tracker_runs)
     global_size = len(global_runs)
 
+    tracker_column_count = len(list(tracker_runs))
+    global_column_count = len(list(global_runs))
+
+    assert global_column_count > tracker_column_count
+
     merged = merge_runreg_runreg(tracker_runs, global_runs)
-    assert len(merged) == tracker_size + global_size
-    assert len(tracker_runs) == tracker_size
-    assert len(global_runs) == global_size
+
+    assert len(merged) == tracker_size + global_size, "Line Count increased"
+    assert len(list(merged)) == global_column_count, "Column Count is maximum"
+    assert len(tracker_runs) == tracker_size, "Nothing changed in tracker runs"
+    assert len(global_runs) == global_size, "Nothing changed in global runs"
+    assert global_column_count > tracker_column_count
 
 
 def test_merge_runreg_oms():
-    pass
+    tracker_runs = load_tracker_runs()
+    oms = load_oms_runs()
+
+    tracker_size = len(tracker_runs)
+    tracker_column_count = len(list(tracker_runs))
+    oms_column_count = len(list(oms))
+
+    assert 36 == tracker_column_count
+    assert 35 == oms_column_count
+
+    merged = merge_runreg_oms(tracker_runs, oms)
+
+    merged_column_count = len(list(merged))
+
+    assert len(tracker_runs) == tracker_size, "No Change in Line count"
+    assert (
+        tracker_column_count + oms_column_count - 1 == merged_column_count
+    ), "Increased Column count"
 
 
-def test_merge_runreg_tkdqmdoc():
-    pass
+def test_merge_runreg_oms():
+    tracker_runs = load_tracker_runs()
+    tkdqm = load_tkdqmdoctor_runs()
+
+    tracker_size = len(tracker_runs)
+    tracker_column_count = len(list(tracker_runs))
+
+    assert 36 == tracker_column_count
+
+    merged = merge_runreg_tkdqmdoc(tracker_runs, tkdqm)
+    merged_column_count = len(list(merged))
+
+    assert len(tracker_runs) == tracker_size, "No Change in Line count"
+    assert tracker_column_count + 1 == merged_column_count, "Increased Column count"
---
+++ trackerstudies/merge.py
@@ -0,0 +1,30 @@
+import pandas
+
+
+def merge_runreg_runreg(runreg_dataframe1, runreg_dataframe2):
+    return runreg_dataframe1.append(runreg_dataframe2, sort=True)
+
+
+def merge_runreg_oms(runreg_dataframe, oms_dataframe):
+    rr = runreg_dataframe.set_index(["run_number"])
+    oms = oms_dataframe.set_index(["run_number"])
+    return pandas.merge(
+        rr, oms, how="left", left_on=["run_number"], right_on=["run_number"]
+    ).reset_index()
+
+
+def merge_runreg_tkdqmdoc(runreg_dataframe, tkdqmdoc_dataframe):
+    rr = runreg_dataframe.set_index(["run_number", "reco"])
+    tkdqm = tkdqmdoc_dataframe[["run_number", "reco", "reference_run_number"]]
+    tkdqm = tkdqm.set_index(["run_number", "reco"])
+
+    # Remove Duplicates
+    tkdqm = tkdqm[~tkdqm.index.duplicated(keep="first")]
+
+    return pandas.merge(
+        rr,
+        tkdqm,
+        how="left",
+        left_on=["run_number", "reco"],
+        right_on=["run_number", "reco"],
+    ).reset_index()
